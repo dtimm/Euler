87aef5f6768401478d706542f6ba00bacd57b5a8
David Timm <papa@raspberrypi>
2014-09-22 19:17:26 +0000

    Solved 024 in C.

--- euler024.c
+++ euler024.c
@@ -12,18 +12,80 @@
  * David Timm 2014-09-21
  */
 
+#define NUMBER {0,1,2,3,4,5,6,7,8,9}
+#define LENGTH 10
+#define PERMUTATIONS 1000000
+
 #include <stdio.h>
 
-void find_k(int *);
-void find_l(int *);
+int find_k(int *, int);
+int find_l(int *, int, int);
+void reverse(int *, int);
 
 int main(void)
 {
-  int permutation = 1;
+  int i, j, k, l, temp;
+  int number[LENGTH] = NUMBER;
+
+	for(i = 1; i < PERMUTATIONS; i++)
+	{
+		k = find_k(number, LENGTH);
+		l = find_l(number, LENGTH, k);
+
+		// printf("%d %d\n", k, l);
+
+		temp = number[k];
+		number[k] = number[l];
+		number[l] = temp;
+		reverse(&number[k+1], LENGTH - (k + 1));
+
+	  for(j = 0; j < LENGTH; j++)
+	  {
+			printf("%d", number[j]);
+		}
+
+		printf("\n");
+	}
+}
+
+int find_k(int * word, int len)
+{
   int i;
-  int number[10] = {0,1,2,3,4,5,6,7,8,9};
+  int k = 0;
+  for(i = 0; i < len - 1; i++)
+  {
+		if(word[i] < word[i+1]) k = i;
+  }
 
+	return k;
+}
 
+int find_l(int * word, int len, int k)
+{
+	int i, l;
+	l = k;
+	for(i = k + 1; i < len; i++)
+	{
+		if(word[i] > word[k]) l = i;
+	}
+
+	return l;
+}
+
+void reverse(int * substr, int len)
+{
+	int i;
+	int temp[len];
+
+	for(i = 0; i < len; i++)
+	{
+		temp[i] = substr[(len-1)-i];
+	}
+
+	for(i = 0; i < len; i++)
+	{
+		substr[i] = temp[i];
+	}
 }
 
 /*Find the largest index k such that a[k] < a[k + 1]. If no such index exists,
