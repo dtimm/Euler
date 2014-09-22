4b4bbe3aa00782a3afe91cc1d32f9e5547e0869c
David Timm <timmmeister@gmail.com>
2014-09-21 18:01:19 -0600

    Done for today.

--- euler024.c
+++ euler024.c
@@ -14,6 +14,9 @@
 
 #include <stdio.h>
 
+void find_k(int *);
+void find_l(int *);
+
 int main(void)
 {
   int permutation = 1;
@@ -23,7 +26,11 @@ int main(void)
 
 }
 
-/*Find the largest index k such that a[k] < a[k + 1]. If no such index exists, the permutation is the last permutation.
+/*Find the largest index k such that a[k] < a[k + 1]. If no such index exists,
+the permutation is the last permutation.
+
 Find the largest index l greater than k such that a[k] < a[l].
+
 Swap the value of a[k] with that of a[l].
+
 Reverse the sequence from a[k + 1] up to and including the final element a[n].*/
