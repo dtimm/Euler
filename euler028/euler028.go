264a56d2f228f2756e0d5f436c491313deaf256b
David Timm <timmmeister@gmail.com>
2014-09-27 12:29:49 -0600

    Solved 028 in Go.

--- euler028/euler028.go
+++ euler028/euler028.go
@@ -6,14 +6,46 @@
  * 19  6  1  2 11
  * 18  5  4  3 12
  * 17 16 15 14 13
- * 
+ *
  * It can be verified that the sum of the numbers on the diagonals is 101.
- * 
+ *
  * What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral formed in the same way?
  *
  * David Timm 2014-09-27
  */
 
+package main
+
 import "fmt"
 
+func main() {
+  // Progressions indicate the next value increase in each direction.
+  drprog := 10
+  dlprog := 12
+  ulprog := 14
+  urprog := 16
+  dr := 3
+  dl := 5
+  ul := 7
+  ur := 9
 
+  // Initial value is of the center and fist layer.
+  width := 3
+  total := 25
+
+  for width < 1001 {
+    dr += drprog
+    drprog += 8
+    dl += dlprog
+    dlprog += 8
+    ul += ulprog
+    ulprog += 8
+    ur += urprog
+    urprog += 8
+
+    total = total + dr + dl + ul + ur
+    width += 2
+  }
+
+  fmt.Printf("%d\n", total)
+}
